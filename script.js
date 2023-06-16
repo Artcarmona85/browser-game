66eb0f4c68aacd0bcc8e3e57ec7391f70157dc89
Artemis <artcarmona85@gmail.com>
2023-06-16 08:13:36 -0500

    browsergame

---
+++ script.js
@@ -0,0 +1,13 @@
+//JS isnt scary lol YOU GOT THIS 
+
+class Tomagotchi {
+    constructor(name) {
+      this.name = name;
+      this.hunger = 0;
+      this.happiness = 0;
+      this.cleanliness = 0;
+      this.health = 100;
+    }
+}
+    // The rest of the code and behaviors can be added after this
+  
